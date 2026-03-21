c722c6cc424aa618bce425ea2e34362bee935b8c
Gino070203 <ginoemch132@gmail.com>
2026-03-21 00:24:45 -0500

    Se integro el nuevo recurso users

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from "express"
 import {bookRouter} from "./routes/books.js"
+import { userRouter } from './routes/users.js'
 import { connectDB } from './Config/connectDB.js'
 
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.json())
 connectDB()
 
 app.use("/books",bookRouter)
+app.use("/users",userRouter)
 
 app.listen(PORT_SERVER,()=>{
     console.log(`Servidor levantado en http://localhost:${PORT_SERVER}`)
