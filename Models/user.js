12b2370d257f7bd4a763a16d3894c2e91e3e25af
Gino070203 <ginoemch132@gmail.com>
2026-03-21 01:09:38 -0500

    Se integro el CRUD

--- Models/user.js
+++ Models/user.js
@@ -7,5 +7,20 @@ export class userModel{
         return await usuarios.create(user)
     }
 
+    static async getAll(){
+        return await usuarios.find(); 
+    }
+
+    static async getOne(email){
+        return await usuarios.findOne(email)
+    }
+
+    static async updateUser(id,user){
+        return await usuarios.findOneAndUpdate({_id : new mongoose.Types.ObjectId(id)},user)
+    }
+
+    static async deleteBook(id){
+        return await usuarios.findOneAndDelete({_id: new mongoose.Types.ObjectId(id)})
+    }   
 
 }
