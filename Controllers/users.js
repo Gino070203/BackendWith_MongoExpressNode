0007469efb695d79f2674fc67122cc0ac56e8204
Gino070203 <ginoemch132@gmail.com>
2026-03-21 01:11:50 -0500

    Se integro la encriptacion(con bcrypt) de la clave del usuario, cuando realize un registro

--- Controllers/users.js
+++ Controllers/users.js
@@ -1,14 +1,23 @@
 import {userModel} from "../Models/user.js"
+import bcrypt from "bcrypt"
 
 export class usersController{
 
-    static async createUser(request,response){
+    static async register(request,response){
 
         try {
             
             const {email,nombre,telefono,clave} = request.body
 
-            const data = await userModel.createUser({email,nombre,telefono,clave})
+            const usuarioExiste = await userModel.getOne({email})
+
+            if(usuarioExiste){
+                response.status(400).json({error : "el usuario ya existe"})    
+            }
+
+            const claveEncriptada = await bcrypt.hash(clave,10)
+
+            const data = await userModel.createUser({email,nombre,telefono,clave : claveEncriptada})
 
             response.status(200).json(data)
 
