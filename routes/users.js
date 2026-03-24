130761639d03fe96f26c5e1c18660c525cff3189
Gino070203 <ginoemch132@gmail.com>
2026-03-23 23:20:59 -0500

    Se integro las rutas

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 import { usersController } from "../Controllers/users.js";
 
+
 export const userRouter = Router()
 
-userRouter.post("/",usersController.createUser)
+userRouter.post("/register",usersController.register)
+userRouter.post("/login",usersController.login)
